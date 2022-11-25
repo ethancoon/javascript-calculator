82c17c6202d3a906574acea306f72cd801eb398c
Ethan Coon <eethanccoon@gmail.com>
2022-11-24 20:55:49 -0500

    Added styling

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const output = document.querySelector('.calc-output')
+
+
+// Calculations
 function add(num1, num2) {
     return num1 + num2;
 };
@@ -13,3 +17,15 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(num1, operator, num2) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if(operator === '-') {
+        return subtract(num1, num2);
+    } else if(operator === '*') {
+        return multiply(num1, num2);
+    } else if(operator === '/') {
+        return divide(num1, num2);
+    }
+};
